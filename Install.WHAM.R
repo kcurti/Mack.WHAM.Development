d5290e4f4285f56495fef9ba713013c642059b5b
Kiersten.Curti <1401762007113302@mil>
2025-01-29 16:03:28 -0500

    Update Install.WHAM.R
    Additional information about adding a specific commit and handling libPaths conflicts

--- Install.WHAM.R
+++ Install.WHAM.R
@@ -5,6 +5,8 @@
 
 # Install "pak" package if not already installd
 install.packages("pak")
+install.packages("mnormt")  # Need for plot wham function
+install.packages("Hmisc")  # Need for plot wham function
 
 # Use the "pak" package to install the dev branch of wham:
 pak::pkg_install("timjmiller/wham@devel", lib="C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_devel_6146b5d")
@@ -17,6 +19,14 @@ library("wham", lib.loc = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4
 # Reinstall to streamline directory name
 pak::pkg_install("timjmiller/wham@6146b5d", lib="C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_6146b5d")
 library("wham", lib.loc = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_6146b5d")
+
+# Install new commit (23 Jan 2025)
+pak::pkg_install("timjmiller/wham@168117c", lib="C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_168117c")
+library("wham", lib.loc = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_168117c")
+
+### To avoid having to add different packages to the main library folder, may have to add the specific wham commit folder to the list of library paths using .libPaths()
+.libPaths()
+
 
 
 ### Option 2: Devtools
