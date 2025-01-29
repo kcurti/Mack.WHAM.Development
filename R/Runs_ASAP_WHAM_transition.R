7e9b8b7bf5f7bfb09e00b69084e093d734a9dded
Kiersten.Curti <1401762007113302@mil>
2025-01-29 16:04:34 -0500

    Update Runs_ASAP_WHAM_transition.R
    Initial cut of Run 2, tweaks to Run 1 and ASAP prep.  Would like to streamline but still trying to determine best workflow

--- R/Runs_ASAP_WHAM_transition.R
+++ R/Runs_ASAP_WHAM_transition.R
@@ -1,7 +1,6 @@
 ### Runs to transition from ASAP to WHAM
 
-# pak::pkg_install("timjmiller/wham@lab", lib = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.3/multi_wham")
-library("wham", lib.loc = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.3/multi_wham")
+library("wham", lib.loc = "C:/Users/Kiersten.Curti/AppData/Local/R/win-library/4.4/wham_168117c")
 library(kableExtra)
 require(tidyverse)
 
@@ -21,6 +20,7 @@ source(file.path("R","compare_asap_wham_ests.R"))
 rungroup.dir <- "Runs.ASAP.WHAM.Transition"
 
 
+
 ##### ASAP #####
 
 # Read in 2023 ASAP file used in final 2023MT run, modified to meet WHAM requirements
@@ -36,16 +36,30 @@ mt2023.rdat <- dget("2023.MT.ASAP/ASAP.files/Run9.RDAT")
 age.vec <- as.character(1:mt2023.orig[[1]]$dat$n_ages)
 nage <- length(age.vec)
 
+# Explore indices and index selectivity parameters
 mt2023.orig[[1]]$dat$"index.names"
 mt2023.init.sel.fish <- as_tibble(mt2023.rdat$sel.input.mats$fleet.sel.ini[,1]) %>% filter(value>0) 
 
-
-# Total number of initial index selectivity parameters
+# Total number of initial index selectivity parameters, initial values and phases from ASAP
 ninit.indexsel <- length(mt2023.rdat$sel.input.mats$index.sel.ini[,2])/length(index.names)
 mt2023.sel.index.init <- lapply(split(mt2023.rdat$sel.input.mats$index.sel.ini[,1], rep(1:3, each=ninit.indexsel)),
                                 as_tibble)
 mt2023.sel.index.phase <- lapply(split(mt2023.rdat$sel.input.mats$index.sel.ini[,2], rep(1:3, each=ninit.indexsel)),
                                 as_tibble)
+
+# ASAP index selectivity estimates
+mt2023.sel.index.values <- mt2023.rdat$index.sel
+
+
+
+##### Alex's SSRT Run1 outputs #####
+
+# Read in NAA estimates from Alex's original SSRT Run1 (to use for future comparisons)
+ssrt_run1.dir <- c("C:/Users/Kiersten.Curti/Documents/GitHub.Repositories/Mack.WHAM.Development/Runs_SSRT/run1")
+ssrt_run1.params <- readRDS(file.path(ssrt_run1.dir, "res_tables", "parameter_estimates_table.RDS"))
+dim(ssrt_run1.params)
+ssrt_run1.NAA <- readRDS(file.path(ssrt_run1.dir, "res_tables", "NAA_table.RDS"))
+dim(ssrt_run1.NAA)
 
 
 
@@ -74,32 +88,76 @@ run1.sel[[1]][1,] # Fishery
 run1.sel[[3]][1,] # Big
 run1.sel[[4]][1,] # Alb
 
+dim(run1$report()$NAA)
+run1.NAA <- run1$report()$NAA[1,1,,]
+dim(run1.NAA)
+head(run1.NAA)
 
 
-##### Run2: Fix albatross selectivity #####
+# Compare my Run1 NAA to SSRT Run1 NAA - there are differences :(
+diff_run1.NAA <- run1.NAA - ssrt_run1.NAA
+head(diff_run1.NAA)
+head(run1.NAA)
+head(ssrt_run1.NAA)
 
-run2.input <- run1.input
 
-run2.sel.spec=list(model=c("age-specific",
-                           "age-specific",
-                           "age-specific",
-                           "age-specific"), 
+
+rm(list=ls()[grep("^asap",ls())])
+
+### Take-home points
+#   1) My run 1 is different than ASAP output
+#   2) My run 1 is also different than Alex's run 1, though Alex changed the ESS's in Run1
+
+
+
+##### Run2: Fix albatross selectivity for ages 9 and 10, bigelow selectivity for age-7 to mimic the ASAP parameterization #####
+
+run.name <- 'run2'
+
+run2.sel.spec=list(model=c("age-specific",  # Fishery
+                           "age-specific",  # SSB egg index
+                           "age-specific",  # Bigelow
+                           "age-specific"), # Albatross
           initial_pars=list(c(0.1,0.5,0.8,1,1,1,1,1,1,1),
                             c(1,1,1,1,1,1,1,1,1,1),
-                            c(0, 0, 1, 0.6, 0.4, 0.2, 0, 0, 0, 0),
-                            c(0, 0, 1, 0.6, 0.4, 0.2, 0, 0, 0, 0)),
+                            c(0, 0, 1, 0.6, 0.4, 0.3, 0.3, 0, 0, 0),
+                            c(0, 0, 1, 0.6, 0.4, 0.3, 0.3, 0.2, mt2023.sel.index.values[3,9:10])),
           fix_pars = list(
             c(6:10),
             c(1:10),
             c(1:3,7:10),
-            c(1:3,7:10))
+            c(1:3,9:10))
 ) 
 
-input_1 <- prepare_wham_input(y, 
-                              selectivity=sel
-)
+# # Does not work
+# run2.input <- run1.input
+# run2.input$options$selectivity = run2.sel.spec
+# run2.input$options$selectivity$initial_pars
 
 
+run2.input <- prepare_wham_input(run1.asap, selectivity = run2.sel.spec)
+
+# Run model
+run2 <- fit_wham(run2.input, do.osa = F, do.retro = F)
+check_convergence(run2)
+
+# Save results and plot output
+run2.dir <- file.path(rungroup.dir, "run2")
+if(!dir.exists(run2.dir)) {dir.create(run2.dir)}
+plot_wham_output(run2, dir.main=file.path(getwd(),run2.dir))
+saveRDS(run2, file=file.path(run2.dir, "run2.rds"))
+
+# Extract time series estimates for comparison to ASAP output
+run2.ests <- extract_time_series_ests(run2.dir, "run2.rds")
+
+# Compare WHAM run to ASAP output
+compare_asap_wham_ests(mt2023.ests, run2.ests, file.path(run2.dir, "Comparison.figures")) 
+
+compare_wham_models(list(run1, run2), fdir=file.path(run2.dir, "Comparison.figures"))
+
+#####
+
+# Save output
 
 
 
